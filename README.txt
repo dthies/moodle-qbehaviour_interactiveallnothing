702cb56ec5d7c1d36838c4517582934c22c3d690
Daniel Thies <dthies@ccal.edu>
2015-05-17 13:09:16 -0500

    Initial version

---
+++ README.txt
@@ -0,0 +1,16 @@
+qbehaviour_adaptiveallnothing
+=================
+
+This is a question behaviour for Moodle Quiz that forces all questions
+to be graded all or nothing using interactive no penalty mode. It will
+work with a variety of question types that normally produce credit
+for partially correct responses i.e. cloze, matching, OU drag and drop
+types etc.
+
+Copy this directory to question/behaviour/deferredallnothing in Moodle
+director. Login as admin to complete plugin installation.  Then choose
+this behaviour while editng quiz settings. All questions in the quiz
+will then always will receive zero or maximum marks when graded.
+
+All original files are copyright Daniel Thies 2015 dthies@ccal.edu and
+are licensed under the included GPL 3
---
+++ renderer.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Renderer for outputting parts of a question belonging to adaptive
+ * all or nothing behaviour.
+ *
+ * @package    qbehaviour
+ * @subpackage adaptiveallnothing
+ * @copyright  2015 Daniel Thies <dthies@ccal.edu>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once(dirname(__FILE__) . '/../adaptive/renderer.php');
+
+class qbehaviour_adaptiveallnothing_renderer extends qbehaviour_adaptive_renderer {
+    protected function grading_details(qbehaviour_adaptive_mark_details $details, question_display_options $options) {
+        $mark = $details->get_formatted_marks($options->markdp);
+        if ($mark['raw'] !== $mark['max']) {
+            $mark['raw'] = 0;
+        }
+        return get_string('gradingdetails', 'qbehaviour_adaptive', $mark);
+    }
+
+    protected function disregarded_info() {
+        return '';
+    }
+}
