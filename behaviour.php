6b609a057dced4b5ebc60c9042c1a642ccf99afc
Daniel Thies <dethies@gmail.com>
2020-03-29 20:51:12 -0500

    Corrent marks calculation

--- behaviour.php
+++ behaviour.php
@@ -37,11 +37,6 @@ require_once(dirname(__FILE__) . '/../interactive/behaviour.php');
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class qbehaviour_interactiveallnothing extends qbehaviour_interactive {
-    protected function adjusted_fraction($fraction, $prevtries) {
-        return question_state::graded_state_for_fraction($fraction) ==
-                question_state::$gradedright ? $fraction - $this->question->penalty * $prevtries: 0;
-    }
-
     public function get_state_string($showcorrectness) {
         if ($this->qa->get_state()->is_partially_correct()) {
             return question_state::$gradedwrong->default_string($showcorrectness);
@@ -49,27 +44,12 @@ class qbehaviour_interactiveallnothing extends qbehaviour_interactive {
         return $this->qa->get_state()->default_string($showcorrectness);
     }
 
-    public function get_interactive_marks() {
-        $gradedstep = $this->get_graded_step();
-
-        // If not partially correct fall back to parent.
-        if (empty($gradedstep) ||
-                question_state::graded_state_for_fraction($gradedstep->get_behaviour_var('_rawfraction'))
-                     == question_state::$gradedright) {
-            return parent::get_interactive_marks();
+    protected function adjust_fraction($fraction, question_attempt_pending_step $pendingstep) {
+        $state = $pendingstep->get_state();
+        if ($state == question_state::$gradedright) {
+            return parent::adjust_fraction($fraction, $pendingstep);
+        } else {
+            return 0;
         }
-
-        // Set state to wrong answer since it is partially correct.
-        $state = question_state::$gradedwrong;
-
-        // Prepare the grading details.
-        $details = $this->interactive_mark_details_from_step($gradedstep, $state, $this->qa->get_max_mark(), $this->question->penalty);
-
-        // Override raw score to show no credit.
-        $details->rawmark = 0;
-
-        $details->improvable = $this->is_state_improvable($this->qa->get_state());
-        return $details;
     }
-
 }
